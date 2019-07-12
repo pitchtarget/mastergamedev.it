82ec8de00965dcaa947ac7cdd710756580fbdc42
Mirco Cotugno <mirco@pitchtarget.com>
2019-07-12 13:17:10 +0100

    students page created with a basic data structure

---
+++ src/templates/students-page.js
@@ -0,0 +1,140 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+
+export const StudentsPageTemplate = ({
+  image,
+  title,
+  heading,
+  description,
+  students,
+}) => {
+  console.log(students)
+  return (
+  <div className="content">
+    <div
+      className="full-width-image-container margin-top-0"
+      style={{
+        backgroundImage: `url(${
+          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+        })`,
+      }}
+    >
+      <h2
+        className="has-text-weight-bold is-size-1"
+        style={{
+          boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
+          backgroundColor: '#f40',
+          color: 'white',
+          padding: '1rem',
+        }}
+      >
+        {title}
+      </h2>
+    </div>
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="section">
+          <div className="columns">
+            <div className="column is-7 is-offset-1">
+              <h3 className="has-text-weight-semibold is-size-2">{heading}</h3>
+              <p>{description}</p>
+            </div>
+          </div>
+          <div className="columns">
+            <div className="column is-10 is-offset-1">
+              <div className="columns">
+                {
+                  students.length > 0 && students.map((student, i) => (
+                    <div key={i} className="column is-4 is-multiline">
+                      <img
+                        src={`${!!student.image.childImageSharp ? student.image.childImageSharp.fluid.src : student.image}`}
+                        alt={student.altImage}
+                      />
+                      <p>
+                        {student.fullName}<br/>
+                        {student.master}<br/>
+                        {student.company}<br/>
+                        {student.role}<br/>
+                        {student.description}<br/>
+                      </p>
+                    </div>
+                  ))
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+)}
+
+StudentsPageTemplate.propTypes = {
+  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  title: PropTypes.string,
+  heading: PropTypes.string,
+  description: PropTypes.string,
+  students: PropTypes.array
+}
+
+const StudentsPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark
+
+  return (
+    <Layout>
+      <StudentsPageTemplate
+        image={frontmatter.image}
+        title={frontmatter.title}
+        heading={frontmatter.heading}
+        description={frontmatter.description}
+        students={frontmatter.students}
+      />
+    </Layout>
+  )
+}
+
+StudentsPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
+}
+
+export default StudentsPage
+
+export const studentsPageQuery = graphql`
+  query StudentsPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        title
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        heading
+        description
+        students {
+          fullName
+          master
+          company
+          role
+          description
+          image {
+            childImageSharp {
+              fluid(maxWidth: 300, quality: 80) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          altImage
+        }
+      }
+    }
+  }
+`
