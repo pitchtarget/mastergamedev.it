99711555a3ab5a69006f6f61dbad8fba4a8679cc
Mirco Cotugno <mirco@pitchtarget.com>
2019-07-15 18:27:57 +0100

    add to home page a filtered list of partners

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
+import { v4 } from 'uuid'
 
 import Layout from '../components/Layout'
 import Features from '../components/Features'
@@ -14,106 +15,115 @@ export const IndexPageTemplate = ({
   mainpitch,
   description,
   intro,
-}) => (
-  <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+  partners,
+  }) => {
+    const filteredPartners = partners.filter(partner => partner.main )
+    return (
+      <div>
+        <div
+          className="full-width-image margin-top-0"
           style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
+            backgroundImage: `url(${
+              !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+            })`,
+            backgroundPosition: `top left`,
+            backgroundAttachment: `fixed`,
           }}
         >
-          {title}
-        </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {subheading}
-        </h3>
-      </div>
-    </div>
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                  <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
-                  </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
+          <div
+            style={{
+              display: 'flex',
+              height: '150px',
+              lineHeight: '1',
+              justifyContent: 'space-around',
+              alignItems: 'left',
+              flexDirection: 'column',
+            }}
+          >
+            <h1
+              className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+              style={{
+                boxShadow:
+                  'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
+                backgroundColor: 'rgb(255, 68, 0)',
+                color: 'white',
+                lineHeight: '1',
+                padding: '0.25em',
+              }}
+            >
+              {title}
+            </h1>
+            <h3
+              className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
+              style={{
+                boxShadow:
+                  'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
+                backgroundColor: 'rgb(255, 68, 0)',
+                color: 'white',
+                lineHeight: '1',
+                padding: '0.25em',
+              }}
+            >
+              {subheading}
+            </h3>
+          </div>
+        </div>
+
+        <section className="section section--gradient">
+          <div className="container">
+            <div className="section">
+              <div className="columns">
+                <div className="column is-10 is-offset-1">
+                  <div className="content">
+                    <div className="content">
+                      <div className="tile">
+                        <h1 className="title">{mainpitch.title}</h1>
+                      </div>
+                      <div className="tile">
+                        <h3 className="subtitle">{mainpitch.description}</h3>
+                      </div>
+                    </div>
+                    <div className="columns">
+                      <div className="column is-12">
+                        <h3 className="has-text-weight-semibold is-size-2">
+                          {heading}
+                        </h3>
+                        <p>{description}</p>
+                      </div>
+                    </div>
+                    { filteredPartners.length > 0 && filteredPartners.map( partner => {
+                        return (
+                          <div key={v4()}>{partner.name}</div>
+                      )})
+                    }
+                    <Features gridItems={intro.blurbs} />
+                    <div className="columns">
+                      <div className="column is-12 has-text-centered">
+                        <Link className="btn" to="/products">
+                          See all products
+                        </Link>
+                      </div>
+                    </div>
+                    <div className="column is-12">
+                      <h3 className="has-text-weight-semibold is-size-2">
+                        Latest stories
+                      </h3>
+                      <BlogRoll />
+                      <div className="column is-12 has-text-centered">
+                        <Link className="btn" to="/blog">
+                          Read more
+                        </Link>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </section>
       </div>
-    </section>
-  </div>
-)
-
+    )
+}
 IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   title: PropTypes.string,
@@ -121,6 +131,7 @@ IndexPageTemplate.propTypes = {
   subheading: PropTypes.string,
   mainpitch: PropTypes.object,
   description: PropTypes.string,
+  partners: PropTypes.array,
   intro: PropTypes.shape({
     blurbs: PropTypes.array,
   }),
@@ -128,6 +139,7 @@ IndexPageTemplate.propTypes = {
 
 const IndexPage = ({ data }) => {
   const { frontmatter } = data.markdownRemark
+  const partnersList = data.partnersData.frontmatter
 
   return (
     <Layout>
@@ -139,6 +151,7 @@ const IndexPage = ({ data }) => {
         mainpitch={frontmatter.mainpitch}
         description={frontmatter.description}
         intro={frontmatter.intro}
+        partners={partnersList.partners}
       />
     </Layout>
   )
@@ -146,6 +159,7 @@ const IndexPage = ({ data }) => {
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
+    partnersData: PropTypes.object,
     markdownRemark: PropTypes.shape({
       frontmatter: PropTypes.object,
     }),
@@ -156,6 +170,23 @@ export default IndexPage
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
+    partnersData: markdownRemark(frontmatter: { templateKey: { eq: "partners-page"}}) {
+      frontmatter {
+        partners {
+          name
+          main
+          link
+          altImage
+          image {
+            childImageSharp {
+              fluid(maxWidth: 300, quality: 80) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         title
