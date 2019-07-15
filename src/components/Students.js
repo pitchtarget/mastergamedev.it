86d0b97e3a891707ecc7f50d4651b257d44ecd31
Mirco Cotugno <mirco@pitchtarget.com>
2019-07-15 09:36:00 +0100

    fix error adding new student

--- src/components/Students.js
+++ src/components/Students.js
@@ -2,25 +2,35 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { v4 } from 'uuid'
 
-const Students = ({ students }) => (
-  <div className="columns is-multiline">
-    {students.length > 0 && students.map(student => (
-      <div key={v4()} className="column is-4">
-        <img
-          src={`${!!student.image.childImageSharp ? student.image.childImageSharp.fluid.src : student.image}`}
-          alt={student.altImage}
-        />
-        <p>
-          {student.fullName}<br/>
-          {student.master}<br/>
-          {student.company}<br/>
-          {student.role}<br/>
-          {student.description}<br/>
-        </p>
-      </div>
-    ))}
-  </div>
-)
+const Students = ({ students }) => {
+  return (
+    <div className="columns is-multiline">
+      {students.length > 0 && students.map(student => {
+        let image
+        if(student.image) {
+          image = !!student.image.childImageSharp ? student.image.childImageSharp.fluid.src : student.image
+        } else {
+          image = '/img/student-img.png'
+        }
+
+        return (
+        <div key={v4()} className="column is-4">
+          <img
+            src={`${image}`}
+            alt={student.altImage}
+          />
+          <p>
+            {student.fullName}<br/>
+            {student.master}<br/>
+            {student.company}<br/>
+            {student.role}<br/>
+            {student.description}<br/>
+          </p>
+        </div>
+      )})}
+    </div>
+  )
+}
 
 Students.propTypes = {
   students: PropTypes.arrayOf(
