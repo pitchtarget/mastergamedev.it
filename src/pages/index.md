6a326ac475aebc5928645c2e6ec67403d9754356
Mirco Cotugno <mirco@pitchtarget.com>
2019-07-29 20:04:46 +0200

    WIP add basic homepage template, add entries to cms

--- src/pages/index.md
+++ src/pages/index.md
@@ -4,6 +4,72 @@ title: Great coffee with a conscience
 image: /img/home-jumbotron.jpg
 heading: Great coffee with a conscience
 subheading: Support sustainable farming while enjoying a cup
+
+mainCover:
+  image: /img/home-jumbotron.jpg
+  heading: Titolo della copertina
+  subheading: Quì la descrizione della copertiona
+  cta: Scopri di più
+  link: /school
+
+topSignIn:
+  image: /img/home-jumbotron.jpg
+  heading: Partecipa all'open day di ottobre
+  subheading: Duarnte l'open day potrai cosnoscere i docenti ed ascoltare le esperienze degli studenti
+  cta: Scopri di più
+  link: /openday
+
+partnersSection:
+  heading: Fatti conoscere dallle aziende leader nel settore del gaming
+  subheading: In stretta collaborazione con il Master di Verona ci sono le raltà più influenti della scena italiana
+  cta: Vedi tutte
+  link: /school
+
+schoolSection:
+  image: /img/home-jumbotron.jpg
+  heading: Un master che fa la differenza
+  subheading: Descrione del master...
+  cta: Vedi il programma
+  link: /school
+  bullet:
+    - heading: Un master universitario
+      subheading: Il master di game development è un percorso formativo di livello universitario
+      cta: continua
+      link: /school
+    - heading: Docenti professionisti del settore
+      subheading: Al Master acquisirai le conoscenze da un gruppo di docenti professionisti del game development
+      cta: continua
+      link: /school
+    - heading: Un filo diretto con le aziende leader del settore
+      subheading: La scuola è uno dei bacini più interessanti per le aziende di questo settore
+      cta: continua
+      link: /school
+
+studentsSection:
+  heading: Una scuola che ti apre le porte a mondo del lavoro
+  subheading: I nostri studenti lavorano su progetti che li portano su strade diverse
+  cta: Vedi i progetti
+  link: /students
+
+bottomSignIn:
+  image: /img/home-jumbotron.jpg
+  heading: Iscriviti all'openday
+  subheading: Durante l'open day vengono organizzati brevi work shop con i ragazzi del master che ti aiuteranno a creare il tuo primo gioco
+  cta: partecipa ora
+  link: /openday
+
+teachersSection:
+  heading: Un dream team, più che un gruppo di docenti
+  subheading: Tra i docenti potrai conoscere giavani e appassionatissimi  universitari ma anche professionisti del settore sempre aggiornati sulle nuove tecnologie
+  cta: Vedi tutti i docenti
+  link: /school
+
+newsSection:
+  heading: Le ultime dal master
+  cta: Vedi tutte
+  link: /blog
+
+
 mainpitch:
   title: Why Kaldi
   description: >
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -4,158 +4,217 @@ import { Link, graphql } from 'gatsby'
 import { v4 } from 'uuid'
 
 import Layout from '../components/Layout'
-import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
 import PartnerCard from '../components/cards/partnerCard'
 
 export const IndexPageTemplate = ({
-  image,
-  title,
-  heading,
-  subheading,
-  mainpitch,
-  description,
-  intro,
+  mainCover,
+  topSignIn,
+  partnersSection,
+  schoolSection,
+  studentsSection,
+  bottomSignIn,
+  teachersSection,
+  newsSection,
   partners,
-  }) => {
+  students,
+  teachers,
+}) => {
     const filteredPartners = partners.filter(partner => partner.main)
+    const filteredStudents = students.filter(student => student.main)
+    const teachersNum = teachers.length
+    const randomTeachers = []
+    // to be create a random filter for teachers
+
     return (
-      <div>
-        <div
-          className="full-width-image margin-top-0"
-          style={{
-            backgroundImage: `url(${
-              !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-            })`,
-            backgroundPosition: `top left`,
-            backgroundAttachment: `fixed`,
-          }}
-        >
-          <div
-            style={{
-              display: 'flex',
-              height: '150px',
-              lineHeight: '1',
-              justifyContent: 'space-around',
-              alignItems: 'left',
-              flexDirection: 'column',
-            }}
-          >
-            <h1
-              className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-              style={{
-                boxShadow:
-                  'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-                backgroundColor: 'rgb(255, 68, 0)',
-                color: 'white',
-                lineHeight: '1',
-                padding: '0.25em',
-              }}
-            >
-              {title}
-            </h1>
-            <h3
-              className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-              style={{
-                boxShadow:
-                  'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-                backgroundColor: 'rgb(255, 68, 0)',
-                color: 'white',
-                lineHeight: '1',
-                padding: '0.25em',
-              }}
-            >
-              {subheading}
+      <>
+        <section className="hero">
+          <div className="container">
+
+            <div id="maincover" className="columns">
+              <div className="column is-5 is-offset-1">
+                <h1 className="content">
+                  <p className="title is-1">{mainCover.heading}</p>
+                  <p className="subtitle is-5">{mainCover.subheading}</p>
+                </h1>
+                {mainCover.cta}
+              </div>
+              <div className="column">
+                <img src={!!mainCover.image.childImageSharp ? mainCover.image.childImageSharp.fluid.src : mainCover.image}/>
+              </div>
+            </div>
+
+            <div id="topSignUpBox" className="">
+              <div className="columns is-gapless">
+                <div className="column is-3">
+                  <img src={!!topSignIn.image.childImageSharp ? topSignIn.image.childImageSharp.fluid.src : topSignIn.image}/>
+                </div>
+                <div className="column">
+                  <h2 className="content">
+                    <p className="title is-5">{topSignIn.heading}</p>
+                    <p className="subtitle is-5">{topSignIn.subheading}</p>
+                  </h2>
+                </div>
+                <div className="column is-3">
+                  {topSignIn.cta}
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </section>
+
+        <section id="partnersSection">
+          <div className="container">
+            <h3 className="content has-text-centered">
+              <p className="title is-2">{partnersSection.heading}</p>
+              <p className="subtitle is-5">{partnersSection.subheading}</p>
             </h3>
           </div>
-        </div>
+          <div className="columns is-centered" style={{overflow: "scroll"}}>
+            { filteredPartners.length > 0 && filteredPartners.map( partner => (
+                <div key={v4()} className="column is-2">
+                  <PartnerCard partner={partner}/>
+                </div>
+              ))
+            }
+          </div>
+          <p className="has-text-centered">{partnersSection.cta}</p>
+        </section>
 
-        <section className="section section--gradient">
+        <section id="schoolSection">
           <div className="container">
-            <div className="section">
-              <div className="columns">
-                <div className="column is-10 is-offset-1">
-                  <div className="content">
-                    <div className="content">
-                      <div className="tile">
-                        <h1 className="title">{mainpitch.title}</h1>
-                      </div>
-                      <div className="tile">
-                        <h3 className="subtitle">{mainpitch.description}</h3>
-                      </div>
-                    </div>
-                    <div className="columns">
-                      <div className="column is-12">
-                        <h3 className="has-text-weight-semibold is-size-2">
-                          {heading}
-                        </h3>
-                        <p>{description}</p>
-                      </div>
-                    </div>
-                    <div className="columns is-centered" style={{overflow: "scroll"}}>
-                      { filteredPartners.length > 0 && filteredPartners.map( partner => (
-                          <div key={v4()} className="column">
-                            <PartnerCard partner={partner}/>
-                          </div>
-                        ))
-                      }
-                    </div>
-                    <Features gridItems={intro.blurbs} />
-                    <div className="columns">
-                      <div className="column is-12 has-text-centered">
-                        <Link className="btn" to="/products">
-                          See all products
-                        </Link>
-                      </div>
-                    </div>
-                    <div className="column is-12">
-                      <h3 className="has-text-weight-semibold is-size-2">
-                        Latest stories
-                      </h3>
-                      <BlogRoll />
-                      <div className="column is-12 has-text-centered">
-                        <Link className="btn" to="/blog">
-                          Read more
-                        </Link>
-                      </div>
+            <div className="columns is-gapeless"
+            style={{
+              backgroundImage: `url(${
+                !!schoolSection.image.childImageSharp ? schoolSection.image.childImageSharp.fluid.src : schoolSection.image
+              })`
+            }}>
+              <div className="column is-4 is-offset-1">
+                <h3 className="content">
+                  <p className="title is-2">{schoolSection.heading}</p>
+                  <p className="subtitle is-5">{schoolSection.subheading}</p>
+                </h3>
+                {schoolSection.cta}
+              </div>
+            </div>
+            <div className="columns">
+              { schoolSection.bullet.length > 0 && schoolSection.bullet.map( bullet =>(
+                  <div key={v4()} className="column is-desktop-3">
+                    <div className="box">
+                      <h4 className="content">
+                        <p className="title is-4">{bullet.heading}</p>
+                        <p className="subtitle is-4">{bullet.subheading}</p>
+                        <p className="">{bullet.cta}</p>
+                      </h4>
                     </div>
                   </div>
+                ))
+              }
+            </div>
+          </div>
+        </section>
+
+        <section id="studentsSection">
+          <div className="container">
+            <div className="columns is-gapeless">
+              <div className="column is-4 is-offset-1">
+                <h3 className="content">
+                  <p className="title is-2">{studentsSection.heading}</p>
+                  <p className="subtitle is-5">{studentsSection.subheading}</p>
+                </h3>
+                {studentsSection.cta}
+              </div>
+            </div>
+            <div className="columns is-centered" style={{overflow: "scroll"}}>
+              { filteredStudents.length > 0 && filteredStudents.map( student =>(
+                  <div key={v4()} className="column is-12">
+                    <div className="box">
+                      <h4 className="content">
+                        <p className="title is-5">{student.fullName}</p>
+                        <p className="subtitle is-5">{student.company}</p>
+                        <p className="">{student.role}</p>
+                        <p className="">{student.description}</p>
+                        <p className="">{student.master}</p>
+                      </h4>
+                    </div>
+                  </div>
+                ))
+              }
+            </div>
+
+            <div id="bottomSignUpBox" className="">
+              <div className="columns is-gapless">
+                <div className="column is-3">
+                  <img src={!!bottomSignIn.image.childImageSharp ? bottomSignIn.image.childImageSharp.fluid.src : bottomSignIn.image}/>
                 </div>
+                <div className="column">
+                  <h2 className="content">
+                    <p className="title is-5">{bottomSignIn.heading}</p>
+                    <p className="subtitle is-5">{bottomSignIn.subheading}</p>
+                  </h2>
+                </div>
+                <div className="column is-3">
+                  {bottomSignIn.cta}
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </section>
+
+        <section id="teachersSection" className="">
+          <div className="container">
+            <div className="columns is-gapeless">
+              <div className="column is-4 is-offset-1">
+                <h3 className="content">
+                  <p className="title is-2">{teachersSection.heading}</p>
+                  <p className="subtitle is-5">{teachersSection.subheading}</p>
+                </h3>
+                {teachersSection.cta}
               </div>
             </div>
           </div>
         </section>
-      </div>
+
+        {/*<BlogRoll />*/}
+      </>
     )
 }
 IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  title: PropTypes.string,
-  heading: PropTypes.string,
-  subheading: PropTypes.string,
-  mainpitch: PropTypes.object,
-  description: PropTypes.string,
+  mainCover: PropTypes.object,
+  topSignIn: PropTypes.object,
+  partnersSection: PropTypes.object,
+  schoolSection: PropTypes.object,
+  studentsSection: PropTypes.object,
+  bottomSignIn: PropTypes.object,
+  teachersSection: PropTypes.object,
+  newsSection: PropTypes.object,
   partners: PropTypes.array,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
+  students: PropTypes.array,
+  teachers: PropTypes.array,
 }
 
 const IndexPage = ({ data }) => {
   const { frontmatter } = data.markdownRemark
   const partnersList = data.partnersData.frontmatter
+  const studentsList = data.studentsData.frontmatter
+  const teachersList = data.teachersData.frontmatter
 
   return (
     <Layout>
       <IndexPageTemplate
-        image={frontmatter.image}
-        title={frontmatter.title}
-        heading={frontmatter.heading}
-        subheading={frontmatter.subheading}
-        mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
+        mainCover={frontmatter.mainCover}
+        topSignIn={frontmatter.topSignIn}
+        partnersSection={frontmatter.partnersSection}
+        schoolSection={frontmatter.schoolSection}
+        studentsSection={frontmatter.studentsSection}
+        bottomSignIn={frontmatter.bottomSignIn}
+        teachersSection={frontmatter.teachersSection}
+        newsSection={frontmatter.newsSection}
         partners={partnersList.partners}
+        students={studentsList.students}
+        teachers={teachersList.teachers}
       />
     </Layout>
   )
@@ -164,6 +223,8 @@ const IndexPage = ({ data }) => {
 IndexPage.propTypes = {
   data: PropTypes.shape({
     partnersData: PropTypes.object,
+    studentsData: PropTypes.object,
+    teachersData: PropTypes.object,
     markdownRemark: PropTypes.shape({
       frontmatter: PropTypes.object,
     }),
@@ -174,6 +235,24 @@ export default IndexPage
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
+    teachersData: markdownRemark(frontmatter: { templateKey: { eq: "teachers"}}) {
+      frontmatter {
+        teachers {
+          fullName
+          role
+          bio
+          link
+          altImage
+          image {
+            childImageSharp {
+              fluid(maxWidth: 300, quality: 80) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
     partnersData: markdownRemark(frontmatter: { templateKey: { eq: "partners"}}) {
       frontmatter {
         partners {
@@ -191,36 +270,108 @@ export const pageQuery = graphql`
         }
       }
     }
-    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+    studentsData: markdownRemark(frontmatter: { templateKey: { eq: "students-page"}}) {
       frontmatter {
-        title
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        heading
-        subheading
-        mainpitch {
-          title
+        students {
+          fullName
+          main
+          master
+          company
+          role
           description
-        }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
+          altImage
+          image {
+            childImageSharp {
+              fluid(maxWidth: 300, quality: 80) {
+                ...GatsbyImageSharpFluid
               }
             }
-            text
+          }
+        }
+      }
+    }
+    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+      frontmatter {
+        mainCover {
+          image {
+            childImageSharp {
+              fluid(maxWidth: 1980, quality: 60) {
+                ...GatsbyImageSharpFluid
+              }
+            }
           }
           heading
-          description
+          subheading
+          cta
+          link
+        }
+        topSignIn {
+          image {
+            childImageSharp {
+              fluid(maxWidth: 300, quality: 80) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          heading
+          subheading
+          cta
+          link
+        }
+        partnersSection {
+          heading
+          subheading
+          cta
+          link
+        }
+        schoolSection {
+          image {
+            childImageSharp {
+              fluid(maxWidth: 1980, quality: 80) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          heading
+          subheading
+          cta
+          link
+          bullet {
+            heading
+            subheading
+            cta
+            link
+          }
+        }
+        studentsSection {
+          heading
+          subheading
+          cta
+          link
+        }
+        bottomSignIn {
+          image {
+            childImageSharp {
+              fluid(maxWidth: 300, quality: 80) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          heading
+          subheading
+          cta
+          link
+        }
+        teachersSection {
+          heading
+          subheading
+          cta
+          link
+        }
+        newsSection {
+          heading
+          cta
+          link
         }
       }
     }
