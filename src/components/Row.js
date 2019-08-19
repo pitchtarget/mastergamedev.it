2a8667a95f70ce5d97281df2d7c2b398147a9663
Mirco Cotugno <mirco@pitchtarget.com>
2019-08-19 11:15:29 +0200

    fix responsive layout styles

--- src/components/Row.js
+++ src/components/Row.js
@@ -3,25 +3,35 @@ import PropTypes from 'prop-types'
 import Image from './elements/Image'
 import Button from './elements/Button'
 
-const Row = ({ data, align, color }) => {
+const Row = ({ data, reverse, color }) => {
   const {image, alt, title, text, cta, link} = data
   return (
     <div className={`row row--${color || "default"}`}>
       <div className="container is-fullhd">
         <div
-          className="columns is-tablet is-vcentered is-gapless"
-          style={{flexDirection: align=='invert' ? 'row-reverse' : 'row'}}
+          className="columns is-tablet is-vcentered is-0"
+          style={{flexDirection: reverse ? 'row-reverse' : 'row'}}
         >
-          <div className="column is-half">
+          <div className="column is-4-tablet is-half-desktop">
             <Image src={image} alt={alt} children />
           </div>
-          <div className="column is-half is-5-widescreen">
+          <div className={`column is-8-tablet is-half-desktop is-5-widescreen ${reverse && 'is-offset-1-widescreen'}`} >
             <div
               className="content"
               style={{padding: '10%'}}
             >
-              {title && <h2 className="title">{title}</h2>}
-              {text && <p className="description">{text}</p>}
+              {title &&
+                <h2>
+                  <span className="title is-spaced is-size-3-mobile is-size-2-tablet">
+                    {title}
+                  </span>
+                </h2>
+              }
+              {text &&
+                <p className="description">
+                  {text}
+                </p>
+              }
               {cta && link &&
                 <Button
                   text={cta}
