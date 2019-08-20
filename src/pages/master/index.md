3b35d5ee4f12d1cdc8f5bf0d40ef110ebe89ae60
Mirco Cotugno <mirco@pitchtarget.com>
2019-08-20 11:13:13 +0200

    add page of the master

---
+++ src/pages/master/index.md
@@ -0,0 +1,40 @@
+---
+templateKey: 'master-page'
+path: /master
+title: Il master 2019
+serviceTitle: I nostri servizi
+paragraphs:
+  - title: Paullum deliquit, ponderibus modulisque suis ratio utitur
+    description: >
+      Phasellus laoreet lorem vel dolor tempus vehicula. A communi observantia non est recedendum. Unam incolunt Belgae, aliam Aquitani, tertiam. Cras mattis iudicium purus sit amet fermentum.
+  - title: Nihil hic munitissimus habendi senatus locus, nihil horum?
+    description: >
+      Unam incolunt Belgae, aliam Aquitani, tertiam. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Quo usque tandem abutere, Catilina, patientia nostra? Fictum,  deserunt mollit anim laborum astutumque! Nec dubitamus multa iter quae et nos invenerat. Magna pars studiorum, prodita quaerimus.
+  - title: Mercedem aut nummos unde unde extricat, amaras.
+    description: >
+      Ullamco laboris nisi ut aliquid ex ea commodi consequat. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. A communi observantia non est recedendum. Qui ipsorum lingua Celtae, nostra Galli appellantur. Petierunt uti sibi concilium totius Galliae in diem certam indicere.
+  - title: Excepteur sint obcaecat cupiditat non proident culpa
+    description: >
+      Me non paenitet nullum festiviorem excogitasse ad hoc. Gallia est omnis divisa in partes tres, quarum. Paullum deliquit, ponderibus modulisque suis ratio utitur. Quisque placerat facilisis egestas cillum dolore. Hi omnes lingua, institutis, legibus inter se differunt. Nec dubitamus multa iter quae et nos invenerat. Ut enim ad minim veniam, quis nostrud exercitation.
+services:
+  - image: /img/service-placeholder.png
+    alt: services
+    heading: Nostrud exercitation
+    subheading: Quid securi etiam tamquam eu fugiat nulla pariatur
+  - image: /img/service-placeholder.png
+    alt: services
+    heading: Nostrud exercitation
+    subheading: Quid securi etiam tamquam eu fugiat nulla pariatur
+  - image: /img/service-placeholder.png
+    alt: services
+    heading: Nostrud exercitation
+    subheading: Quid securi etiam tamquam eu fugiat nulla pariatur
+  - image: /img/service-placeholder.png
+    alt: services
+    heading: Nostrud exercitation
+    subheading: Quid securi etiam tamquam eu fugiat nulla pariatur
+  - image: /img/service-placeholder.png
+    alt: services
+    heading: Nostrud exercitation
+    subheading: Quid securi etiam tamquam eu fugiat nulla pariatur
+---
---
+++ src/templates/master-page.js
@@ -0,0 +1,125 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import SignupBox from '../components/SignupBox'
+import Image from '../components/elements/Image'
+import BlogRoll from '../components/BlogRoll'
+import { v4 } from 'uuid'
+
+export const MasterPageTemplate = ({
+  title,
+  serviceTitle,
+  paragraphs,
+  services,
+}) => {
+  const topParagraphs = paragraphs.length > 0 && paragraphs.slice(0,3)
+  const bottomParagraphs = paragraphs.length > 0 && paragraphs.slice(3)
+
+  return(
+    <div className="content">
+      <section className="">
+        <div className="container">
+          <h1>{title}</h1>
+          { topParagraphs.length > 0 && topParagraphs.map( paragraph =>(
+              <div key={v4()} className="section">
+                <h3>{paragraph.title}</h3>
+                <p>{paragraph.description}</p>
+              </div>
+            ))
+          }
+        </div>
+      </section>
+      <section className="">
+        <div className="container">
+          <h2>{serviceTitle}</h2>
+          <div className="columns is-multiline is-vcentered is-centered">
+            { services.length > 0 && services.map( service =>(
+                <div key={v4()} className="column is-4-tablet is-2-desktop is-1-fullhd has-text-centered">
+                  <Image src={service.image} alt={service.alt} styles="is-96x96" rounded/>
+                  <h4>{service.heding}</h4>
+                  <p><small>{service.subheading}</small></p>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </section>
+      <section className="">
+        <div className="container">
+          { bottomParagraphs.length > 0 && bottomParagraphs.map( paragraph =>(
+              <div key={v4()} className="section">
+                <h3>{paragraph.title}</h3>
+                <p>{paragraph.description}</p>
+              </div>
+            ))
+          }
+        </div>
+      </section>
+      <div className="container">
+        <div className="section">
+          <BlogRoll />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+MasterPageTemplate.propTypes = {
+  title: PropTypes.string,
+  serviceTitle: PropTypes.string,
+  paragraphs: PropTypes.array,
+  services: PropTypes.array,
+}
+
+const MasterPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark
+
+  return (
+    <Layout>
+      <MasterPageTemplate
+        title={frontmatter.title}
+        serviceTitle={frontmatter.serviceTitle}
+        paragraphs={frontmatter.paragraphs}
+        services={frontmatter.services}
+      />
+    </Layout>
+  )
+}
+
+MasterPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
+}
+
+export default MasterPage
+
+export const masterPageQuery = graphql`
+  query MasterPage {
+    markdownRemark(frontmatter: { templateKey: { eq: "master-page"}}) {
+      frontmatter {
+        title
+        serviceTitle
+        paragraphs {
+          title
+          description
+        }
+        services {
+          heading
+          subheading
+          alt
+          image {
+            childImageSharp {
+              fluid(maxWidth: 100, quality: 80) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
