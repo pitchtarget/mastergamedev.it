192cd9f35b6f546d3034855dd47833845446d840
Mirco Cotugno <mirco@pitchtarget.com>
2019-08-20 15:52:14 +0200

    remove useless pages and components

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,9 +2,7 @@ import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 import StudentsPagePreview from './preview-templates/StudentsPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 import PartnersGridPreview from './preview-templates/PartnersGridPreview'
@@ -14,8 +12,6 @@ CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('students', StudentsPagePreview)
 CMS.registerPreviewTemplate('partners', PartnersGridPreview)
 CMS.registerPreviewTemplate('teachers', TeachersGridPreview)
