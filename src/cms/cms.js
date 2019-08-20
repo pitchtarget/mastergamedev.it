ea1192865f9bd0e71290fc9d589db43f165fe773
Mirco Cotugno <mirco@pitchtarget.com>
2019-08-20 17:09:27 +0200

    add master and program page to CMS

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,6 +4,8 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import StudentsPagePreview from './preview-templates/StudentsPagePreview'
+import MasterPagePreview from './preview-templates/MasterPagePreview'
+import ProgramPagePreview from './preview-templates/ProgramPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 import PartnersGridPreview from './preview-templates/PartnersGridPreview'
 import TeachersGridPreview from './preview-templates/TeachersGridPreview'
@@ -16,5 +18,7 @@ CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('students', StudentsPagePreview)
 CMS.registerPreviewTemplate('partners', PartnersGridPreview)
 CMS.registerPreviewTemplate('teachers', TeachersGridPreview)
+CMS.registerPreviewTemplate('master', MasterPagePreview)
+CMS.registerPreviewTemplate('program', ProgramPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('banners', BannersListPreview)
--- src/cms/preview-templates/BannersListPreview.js
+++ src/cms/preview-templates/BannersListPreview.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { BannerList } from '../../components/BannerList'
 
 const BannersListPreview = ({ entry, getAsset }) => {
+  debugger
   const entryBanners = entry.getIn(['data', 'banners'])
   const banners = entryBanners ? entryBanners.toJS() : []
 
