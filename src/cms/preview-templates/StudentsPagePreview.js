b2b63adcad65095fa5bfba07f089a75b88c013cf
Mirco Cotugno <mirco@pitchtarget.com>
2019-08-27 16:55:56 +0200

    Add validation on Row comp

--- src/cms/preview-templates/StudentsPagePreview.js
+++ src/cms/preview-templates/StudentsPagePreview.js
@@ -3,15 +3,12 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import { StudentsPageTemplate } from '../../templates/students-page'
 
-const StudentsPagePreview = ({ entry, getAsset, data }) => {
+const StudentsPagePreview = ({ entry, getAsset }) => {
   const entryStudents = entry.getIn(['data', 'students'])
   const students = entryStudents ? entryStudents.toJS() : []
 
   const entryProjects = entry.getIn(['data', 'projects'])
   const projects = entryProjects ? entryProjects.toJS() : []
-
-  const banners = data.bannersData.frontmatter.banners
-  const banner = banners.filter(banner => banner.name === 'partners')
 
   return (
     <StudentsPageTemplate
@@ -21,7 +18,7 @@ const StudentsPagePreview = ({ entry, getAsset, data }) => {
       description={entry.getIn(['data', 'description'])}
       projects={projects}
       students={students}
-      banner={banner[0]}
+      banner={[]}
     />
   )
 }
@@ -31,37 +28,6 @@ StudentsPagePreview.propTypes = {
     getIn: PropTypes.func,
   }),
   getAsset: PropTypes.func,
-  data: PropTypes.shape({
-    bannersData: PropTypes.shape({
-      frontmatter: PropTypes.shape({
-        banners: PropTypes.array,
-      }),
-    }),
-  }),
 }
 
 export default StudentsPagePreview
-
-export const studentsPagePreviewQuery = graphql`
-  query StudentsPagePreview($id: String!) {
-    bannersData: markdownRemark(frontmatter: { templateKey: { eq: "banners"}}) {
-      frontmatter {
-        banners {
-          name
-          title
-          text
-          cta
-          link
-          alt
-          image {
-            childImageSharp {
-              fluid(maxWidth: 500, quality: 80) {
-                ...GatsbyImageSharpFluid
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
--- src/components/Row.js
+++ src/components/Row.js
@@ -17,46 +17,49 @@ const ctaStyle = (color) => {
 
 const Row = ({ data, reverse, color }) => {
   const {image, alt, title, text, cta, link} = data
-
-  return (
-    <div className={`row row__${color || "default"}`}>
-      <div className="container is-fullhd">
-        <div
-          className="row-wrapper columns is-tablet is-vcentered"
-          style={{flexDirection: reverse ? 'row-reverse' : 'row'}}
-        >
-          <div className="row--image column is-4-tablet is-half-desktop is-5-widescreen">
-            <Image src={image} alt={alt} children/>
-          </div>
-          <div className={`column is-8-tablet is-half-desktop is-5-fullhd ${reverse ? 'is-offset-1-widescreen' : ''}`} >
-            <div
-              className="content"
-              style={{padding: '5%'}}
-            >
-              {title &&
-                <h2>
-                  <span className="title is-spaced is-size-3-mobile is-size-2-tablet">
-                    {title}
-                  </span>
-                </h2>
-              }
-              {text &&
-                <p className="description">
-                  {text}
-                </p>
-              }
-              {cta && link &&
-                <Button
-                  text={cta} link={link}
-                  styles={`cta cta-large cta__${ctaStyle(color)}`}
-                />
-              }
+  if(!data) {
+    return  null
+  } else {
+    return (
+      <div className={`row row__${color || "default"}`}>
+        <div className="container is-fullhd">
+          <div
+            className="row-wrapper columns is-tablet is-vcentered"
+            style={{flexDirection: reverse ? 'row-reverse' : 'row'}}
+          >
+            <div className="row--image column is-4-tablet is-half-desktop is-5-widescreen">
+              <Image src={image} alt={alt} children/>
+            </div>
+            <div className={`column is-8-tablet is-half-desktop is-5-fullhd ${reverse ? 'is-offset-1-widescreen' : ''}`} >
+              <div
+                className="content"
+                style={{padding: '5%'}}
+              >
+                {title &&
+                  <h2>
+                    <span className="title is-spaced is-size-3-mobile is-size-2-tablet">
+                      {title}
+                    </span>
+                  </h2>
+                }
+                {text &&
+                  <p className="description">
+                    {text}
+                  </p>
+                }
+                {cta && link &&
+                  <Button
+                    text={cta} link={link}
+                    styles={`cta cta-large cta__${ctaStyle(color)}`}
+                  />
+                }
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 Row.propTypes = {
   data: PropTypes.shape({
