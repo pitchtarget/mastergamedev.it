e287c0c77660e6f98064697566cf0c45affab7a2
Mirco Cotugno <mirco@pitchtarget.com>
2019-08-29 16:09:34 +0200

    WIP - fix config.yml

--- src/components/SignupBox.js
+++ src/components/SignupBox.js
@@ -6,29 +6,28 @@ import Button from '../components/elements/Button'
 
 export const SignupBoxComponent = ({ data, color, id, styles }) => {
   const style = color === "invert"
-  const content = data
   return (
     <div id={id} className={`signupBox signupBox${style ? "__invert" : "__regular"} ${styles}`}>
       <div className="columns is-desktop is-gapless is-vcentered">
         <div className="column is-3-desktop">
           <div
             className="signupBox--image"
-            style={{backgroundImage: `url(${!!content.image.childImageSharp ? content.image.childImageSharp.fluid.src : content.image})`}}
+            style={{backgroundImage: `url(${!!data.image.childImageSharp ? data.image.childImageSharp.fluid.src : data.image})`}}
           />
         </div>
         <div className="column">
           <div className="columns is-gapless is-vcentered">
             <div className="column">
               <div className={`signupBox--text signupBox--text${style ? "__invert" : "__regular"}`}>
-                <h2 className="title is-4">{content.title}</h2>
-                <p className="subtitle is-6">{content.text}</p>
+                <h2 className="title is-4">{data.title}</h2>
+                <p className="subtitle is-6">{data.text}</p>
               </div>
             </div>
             <div className="column is-4-desktop">
               <div className="signupBox--cta">
                 <Button
-                  text={content.cta}
-                  link={content.link}
+                  text={data.cta}
+                  link={data.link}
                   styles={`cta cta-large cta${!style ? "__invert" : "__primary"}`}
                 />
               </div>
