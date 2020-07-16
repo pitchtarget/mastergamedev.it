65453f589aa286b569ac7719d480a7357d12ef5f
Mirco Cotugno <mirco@pitchtarget.com>
2020-07-16 11:42:38 +0200

    fix studentCard import

--- src/components/Students.js
+++ src/components/Students.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { v4 } from 'uuid'
-import StudentCard from './cards/StudentCard'
+import StudentCard from './cards/studentCard'
 
 const Students = ({ students }) => {
   return (
